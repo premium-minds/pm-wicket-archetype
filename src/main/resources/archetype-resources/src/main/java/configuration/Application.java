4e13f14b94551c87a4fd652cf550a649f0a495fb
André Camilo <andre.camilo@premium-minds.com>
2014-04-17 18:25:43 +0100

    correcao de bugs

--- src/main/resources/archetype-resources/src/main/java/configuration/Application.java
+++ src/main/resources/archetype-resources/src/main/java/configuration/Application.java
@@ -5,6 +5,9 @@ package ${package}.configuration;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.inject.persist.PersistService;
 import com.premiumminds.persistence.PersistenceTransaction;
 import com.premiumminds.webapp.wicket.persistence.TransactionalRequestCycleListener;
@@ -15,19 +18,31 @@ public class Application extends WebApplication {
 	@Inject
 	private PersistService persistService;
 	
+	private static Logger log = LoggerFactory.getLogger(Application.class);
+	
 	@Override
 	protected void init() {
-		persistService.start();
-
-		getRequestCycleListeners().add(new TransactionalRequestCycleListener(persistenceTransaction));
-		
-		super.init();
+		try {
+			persistService.start();
+	
+			getRequestCycleListeners().add(new TransactionalRequestCycleListener(persistenceTransaction));
+			
+			super.init();
+		} catch(Throwable t){
+			log.error("error starting application", t);
+			throw t;
+		}
 	}
 	
 	@Override
 	protected void finalize() throws Throwable {
-		super.finalize();
-		
-		persistService.stop();
+		try {
+			super.finalize();
+			
+			persistService.stop();
+		} catch(Throwable t){
+			log.error("error ending application", t);
+			throw t;
+		}
 	}
 }
